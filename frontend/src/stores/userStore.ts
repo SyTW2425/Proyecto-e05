00dca035c694e09dadc05b41363d0cebb9f81929
alu0101463018 <alu0101463018@ull.edu.es>
2024-11-14 21:56:51 +0000

    install pinia

---
+++ frontend/src/stores/userStore.ts
@@ -0,0 +1,19 @@
+// src/store/userStore.ts
+import { defineStore } from 'pinia';
+
+export const useUserStore = defineStore('user', {
+  state: () => ({
+    user: null,
+  }),
+
+  actions: {
+    setUser(user: any) {
+      this.user = user;
+    },
+    clearUser() {
+      this.user = null;
+    },
+  },
+
+  persist: true,
+});
