b65c4aeea18e4810c5039672cfd759d29b3e21cd
Daniel <alu0101501338@ull.edu.es>
2024-11-20 18:38:10 +0000

    Minor changes to movie model

--- backend/src/models/movieModel.ts
+++ backend/src/models/movieModel.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const movieSchema = new mongoose.Schema({
   title: { type: String, required: true },
   releaseYear: { type: Number, required: true },
-  TMDid: { type: Number, required: true }, //id in the external api
+  TMDid: { type: Number, required: true, unique: true }, //id in the external api
   // we could add more fields or use the external api
 });
 
