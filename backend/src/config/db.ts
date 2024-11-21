af8e5e403ba6dc82a8f3cd182585ad1fe3c31460
Daniel <alu0101501338@ull.edu.es>
2024-11-21 18:18:39 +0000

    New MONGODB_URI, and axios package

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,10 +3,13 @@ dotenv.config(); // Load environment variables
 
 import mongoose from 'mongoose';
 
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
+
+
 // Connect to MongoDB
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI!)
+    await mongoose.connect(process.env.MONGODB_URI!);
     console.log('Connected to MongoDB from db.ts')
   } catch (err) {
     console.error('Error connecting to MongoDB', err);
