2bb62ffe4b1d89fa9a3c728f99ba7571a12b2ce3
Daniel <alu0101501338@ull.edu.es>
2024-11-22 16:17:41 +0000

    Login and Signup routes in backend

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,1 +1,48 @@
-// 
+import { Request, Response } from "express"; 
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { User } from "../models/userModel";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export const register = async (req: Request, res: Response): Promise<void> => {
+  try {
+   const { name, username, password, email } = req.body;
+ 
+   // Check if the username or email is already registered
+   const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+   if (existingUser) {
+     res.status(400).json({ message: 'Usernae or email already exists' });
+   }
+ 
+   // Hash the password
+   const hashedPassword = await bcrypt.hash(password, 10);
+ 
+   // Create and save the new user
+   const newUser = new User({ name, username, password: hashedPassword, email });
+   await newUser.save();
+ 
+   res.status(201).json({ message: 'User registered successfully' });
+  } catch (err) {
+   res.status(500).json({ message: 'Error registering user', err });
+  } 
+ }
+ 
+ export const login = async (req: Request, res: Response): Promise<void> => {
+   try {
+     const { username, password } = req.body;
+ 
+     // Find if the user exists
+     const user = await User.findOne({ username });
+     if (!user) {
+       res.status(401).json({ message: 'Invalid credentials' });
+     }
+ 
+     // Generate a JWT
+     const token = jwt.sign({ id: user._id, username: user.username }, JWT_SECRET, { expiresIn: '1h' });
+ 
+     res.status(200).json({ token });
+   } catch (err) {
+     res.status(500).json({ message: 'Error logging in', err });
+   }
+ };
