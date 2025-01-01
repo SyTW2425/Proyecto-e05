0769738e49a81f3da16fdaf63445e3e2f80b3084
Daniel <alu0101501338@ull.edu.es>
2025-01-01 20:54:55 +0000

    All Activity success tests

--- backend/tests/controllers/activityController.test.ts
+++ backend/tests/controllers/activityController.test.ts
@@ -42,6 +42,8 @@ describe('User Controller Tests', () => {
 
   afterEach(async () => {
     await Activity.deleteMany({});
+    await List.deleteMany({});
+    await Review.deleteMany({});
   });
 
   describe('GET /activity', () => {
@@ -97,6 +99,108 @@ describe('User Controller Tests', () => {
       expect(res.status).toBe(200);
       expect(res.body.length).toBe(1);
       expect(res.body[0].type).toBe(ActivityType.FOLLOW);
+    });
+
+    it('should get user and following activities: add to list', async () => {
+      const user3 = new User({
+        name: 'Test User 3',
+        username: 'testuser3',
+        email: 'testuser3@example.com',
+        password: 'testuser3password',
+      });
+      await user3.save();
+      const listPayload = {
+        name: 'Test List',
+        userId: user._id.toString(),
+      };
+      await request(app).post('/api/lists/create').send(listPayload);
+      const list = await List.findOne({ name: 'Test List' });
+      const moviePayload = {
+        listId: list?._id.toString(),
+        title: 'Test Movie',
+        releaseYear: '2021',
+        TMDid: '597',
+      };
+      await request(app).post('/api/lists/add-movie').send(moviePayload);
+      const followPayload = {
+        userId: user._id.toString(),
+        followId: user3._id.toString(),
+      };
+      await request(app).put(`/api/users/follow`).send(followPayload);
+      const listPayload2 = {
+        name: 'Test List 2',
+        userId: user3._id.toString(),
+      };
+      await request(app).post('/api/lists/create').send(listPayload2);
+      const list2 = await List.findOne({ name: 'Test List 2' });
+      const moviePayload2 = {
+        listId: list2?._id.toString(),
+        title: 'Test Movie 2',
+        releaseYear: '2021',
+        TMDid: '597',
+      };
+      await request(app).post('/api/lists/add-movie').send(moviePayload2);
+      const res = await request(app).get(`/api/activity/all/${user._id.toString()}`);
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBe(3);
+      expect(res.body[0].type).toBe(ActivityType.ADD_TO_LIST);
+      expect(res.body[1].type).toBe(ActivityType.FOLLOW);
+      expect(res.body[2].type).toBe(ActivityType.ADD_TO_LIST);
+    });
+
+    it('should get user and following activities: review', async () => {
+      const user2 = await User.findOne({ username: 'testuser2' });
+      const user3 = await User.findOne({ username: 'testuser3' });
+      const reviewPayload = {
+        title: 'Test Review',
+        body: 'Test Review Body',
+        rating: 5,
+        userId: user._id.toString(),
+        movieId: 597,
+      };
+      const reviewPayload2 = {
+        title: 'Test Review',
+        body: 'Test Review Body',
+        rating: 5,
+        userId: user2?._id.toString(),
+        movieId: 587,
+      };
+      const reviewPayload3 = {
+        title: 'Test Review 2',
+        body: 'Test Review Body 2',
+        rating: 4,
+        userId: user3?._id.toString(),
+        movieId: 597,
+      };
+      await request(app).post('/api/reviews/add-review').send(reviewPayload);
+      await request(app).post('/api/reviews/add-review').send(reviewPayload2);
+      await request(app).post('/api/reviews/add-review').send(reviewPayload3);
+      const res = await request(app).get(`/api/activity/all/${user._id.toString()}`);
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBe(3);
+      expect(res.body[0].type).toBe(ActivityType.REVIEW);
+      expect(res.body[1].type).toBe(ActivityType.REVIEW);
+      expect(res.body[2].type).toBe(ActivityType.REVIEW);
+    });
+
+    it('should get user and following activities: follow', async () => {
+      const user2 = await User.findOne({ username: 'testuser2' });
+      const user3 = await User.findOne({ username: 'testuser3' });
+      const followPayload = {
+        userId: user2?._id.toString(),
+        followId: user3?._id.toString(),
+      };
+      const followPayload2 = {
+        userId: user3?._id.toString(),
+        followId: user2?._id.toString(),
+      };
+      await request(app).put(`/api/users/follow`).send(followPayload);
+      await request(app).put(`/api/users/follow`).send(followPayload2);
+      const res = await request(app).get(`/api/activity/all/${user._id.toString()}`);
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBe(2);
+      expect(res.body[0].type).toBe(ActivityType.FOLLOW);
+      expect(res.body[1].type).toBe(ActivityType.FOLLOW);
     });
 
   });
